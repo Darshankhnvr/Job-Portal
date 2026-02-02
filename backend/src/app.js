2c733b93920a014b6077e77138a4ec2bfa6374ad
Darshan K <darshankgoudahnvr@gmail.com>
2026-02-02 13:10:30 +0530

    set-up

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+dotenv.config()
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"))
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.status(200).json({message: "Welcome to the Job Portal"})
+})
+
+app.get("/health", (req, res) =>{
+    res.status(200).json({status: "OK"})
+})
+
+export default app;
