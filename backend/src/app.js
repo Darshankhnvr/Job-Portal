3b8e76dfde8465312d191f5b3e03e1c8c455dabe
Darshan K <darshankgoudahnvr@gmail.com>
2026-02-02 14:08:30 +0530

    authentication set up is done

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
@@ -10,8 +11,9 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"))
-
 app.use(express.json());
+
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) =>{
     res.status(200).json({message: "Welcome to the Job Portal"})
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { adminLogin, login, register } from "../controllers/auth.controller.js"
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/admin/login", adminLogin)
+
+export default router;
